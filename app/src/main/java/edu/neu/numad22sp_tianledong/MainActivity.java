becf1590ff7faf35456b70bbdea556d0c4ff297f
tianledong <dtllawrence@gmail.com>
2022-01-20 10:57:49 -0500

    add about button

--- app/src/main/java/edu/neu/numad22sp_tianledong/MainActivity.java
+++ app/src/main/java/edu/neu/numad22sp_tianledong/MainActivity.java
@@ -3,6 +3,8 @@ package edu.neu.numad22sp_tianledong;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        Toast.makeText(this, "Name: Tianle Dong\n\nEmail: dong.tia@northeastern.edu\n", Toast.LENGTH_SHORT).show();
     }
 }
