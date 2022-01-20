633693d2f9e4587f2376372054b69d3c43255873
tianledong <dtllawrence@gmail.com>
2022-01-19 23:33:38 -0500

    first commit

---
+++ app/src/main/java/edu/neu/numad22sp_tianledong/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.neu.numad22sp_tianledong;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
